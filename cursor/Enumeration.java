6c9848be4eaf69899dd1e420c1d94b62ca8c5086
dipawali mandaokar <96388133+DIPAWALIMANDAOKA@users.noreply.github.com>
2024-08-05 09:19:17 +0530

    Update Enumeration.java

--- cursor/Enumeration.java
+++ cursor/Enumeration.java
@@ -1,1 +1,27 @@
 
+package Cursor;
+
+
+import java.util.Vector;
+import java.util.Enumeration;
+public class Cursor {
+public static void main(String [] args)
+{
+	
+	
+	Vector v= new Vector();
+	for(int i=0;i<10;i++)
+	{
+		v.addElement(i);
+	}
+	System.out.println(v);
+	
+	//it fetches element one by one
+	Enumeration e = v.elements();
+	while(e.hasMoreElements()) {
+		Integer i = (Integer) e.nextElement(); 
+		System.out.println(i);
+	}
+	
+}
+}
