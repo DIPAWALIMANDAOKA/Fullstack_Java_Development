d6d8f185c910da20907efaf55028033df2894869
dipawali mandaokar <96388133+DIPAWALIMANDAOKA@users.noreply.github.com>
2024-08-05 09:15:27 +0530

    Update hashset.java

--- set./hashset.java
+++ set./hashset.java
@@ -1,1 +1,24 @@
+package Set;
 
+import java.util.HashSet;
+
+public class hashset {
+public static void main(String args[])
+{
+	HashSet hs = new HashSet();
+	hs.add("A");
+	hs.add("B");
+	hs.add("C");
+	hs.add("D");
+	hs.add("C");
+	hs.add(10);
+	hs.add(50);
+	hs.add('C');
+	hs.add("C");
+	
+	System.out.println(hs);
+	System.out.println(hs.add("E"));
+	System.out.println(hs);
+	System.out.println(hs.add("B")); //cannot add duplicates
+}
+}
